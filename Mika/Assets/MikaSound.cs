5786437f0b09d3e5d13b674c1ee018c987d35f12
Rodrigo Guzman <rodrigoguzman1202@gmail.com>
2020-01-09 19:21:37 -0300

    Mika
    tutorial Animación en 2D.
    Quedo bien bonito :)

---
+++ Mika/Assets/MikaSound.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MikaSound : MonoBehaviour
+{
+
+    public AudioSource audio;
+    private AudioClip audioClip;
+
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            if (hit.collider != null && hit.collider.tag == "Mika")
+            {
+                audio = GetComponent<AudioSource>();
+                audio.Play();
+                Debug.Log("HITMika");
+            }
+            else
+            {
+                Debug.Log("Did not hit anything");
+            }
+        }
+    }
+}
---
+++ Mika/Assets/Scripts/ClickManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickManager : MonoBehaviour
+{
+    public AudioSource audio;
+    private AudioClip audioClip;
+
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            if (hit.collider != null && hit.collider.tag == "Gato")
+            {
+                audio = GetComponent<AudioSource>();
+                audio.Play();
+                Debug.Log("HIT");
+            }
+            else
+            {
+                Debug.Log("Did not hit anything");
+            }
+        }
+    }
+}
+
